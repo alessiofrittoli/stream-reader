267e23ac7ed9d4d0a999fe0bce3348db8a1a80aa
Alessio Frittoli <info@alessiofrittoli.it>
2025-12-29 11:55:35 +0100

    fix: removed dynamic imports

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
-import type { Readable, Transform } from 'stream'
+import { Readable, Transform } from 'stream'
+import { clamp } from '@alessiofrittoli/math-utils'
 import type { StreamGenerator } from './types'
 
 
@@ -73,119 +74,112 @@ export type ExtractedBytes = [
  * // `header` is a Buffer of 16 bytes, `restStream` is a the piped `Transform` stream that forward the remaining data to the next piped destination.
  * ```
  */
-export const extractBytesFromReadable = async ( input: Readable, bytes: number ) => {
+export const extractBytesFromReadable = ( input: Readable, bytes: number ) => (
+	new Promise<ExtractedBytes>( ( resolve, reject ) => {
 
-	const { Transform }	= await import( 'stream' )
-	const { clamp }		= await import( '@alessiofrittoli/math-utils' )
+		const bytesToExtract = clamp( bytes, 0, Infinity )
+		/**
+		 * A Buffer containing all the byte data we want to extract.
+		 * 
+		 */
+		let ExtractedData = Buffer.alloc( 0 )
+		/**
+		 * Indicates how many bytes we've read from incoming stream.
+		 * 
+		 */
+		let bytesRead = 0
+		/**
+		 * Indicates whether we successfully extracted required byte data
+		 * and already resolved the current Promise.
+		 */
+		let resolved = false
+		
+		const transform = new Transform( {
+			transform( chunk: Buffer, encoding, callback ) {
 
-	return (
-		new Promise<ExtractedBytes>( ( resolve, reject ) => {
+				if ( bytesRead < bytesToExtract ) {
 
-			const bytesToExtract = clamp( bytes, 0, Infinity )
-			/**
-			 * A Buffer containing all the byte data we want to extract.
-			 * 
-			 */
-			let ExtractedData = Buffer.alloc( 0 )
-			/**
-			 * Indicates how many bytes we've read from incoming stream.
-			 * 
-			 */
-			let bytesRead = 0
-			/**
-			 * Indicates whether we successfully extracted required byte data
-			 * and already resolved the current Promise.
-			 */
-			let resolved = false
-
-			const transform = new Transform( {
-				transform( chunk: Buffer, encoding, callback ) {
-
-					if ( bytesRead < bytesToExtract ) {
-
-						// append to the current `ExtractedData` the incoming chunk.
-						ExtractedData = Buffer.concat( [ ExtractedData, chunk ] )
-						
-						// increment the bytes we've read.
-						bytesRead += chunk.length
-						
-						/**
-						 * Indicates whether we fully extracted the byte data and the last chunk
-						 * also contains part of the data we do not want to extract.
-						 * 
-						 * This may occurs when the received chunk length is not a multiple of the given `bytes`.
-						 * In this case the chunk will contain mixed content:
-						 * - end of the `ExtractedData`.
-						 * - begin of data that we do not want to extract.
-						 */
-						const hasByteLoss = ExtractedData.length > bytesToExtract
-						
-						/**
-						 * A Buffer containing chunk data unrelated with the requested extraction data.
-						 * 
-						 */
-						const bytesLoss = hasByteLoss ? ExtractedData.subarray( bytesToExtract ) : undefined
-						
-						// Cut-off extracted data to make sure it exactly matches the requested `bytes`.
-						ExtractedData = ExtractedData.subarray( 0, bytesToExtract )
-						
-						if ( bytesLoss ) {
-							// send rest of the chunk to the next piped destination.
-							this.push( bytesLoss, encoding )
-						}
-
-						if ( ! resolved && ExtractedData.length === bytesToExtract ) {
-							// resolve the Promise so user can immediately obtain the `ExtractedData`.
-							resolved = true
-							resolve( [ ExtractedData, this ] )
-						}
-
-						// early return with loop-back.
-						return callback()
+					// append to the current `ExtractedData` the incoming chunk.
+					ExtractedData = Buffer.concat( [ ExtractedData, chunk ] )
+					
+					// increment the bytes we've read.
+					bytesRead += chunk.length
+					
+					/**
+					 * Indicates whether we fully extracted the byte data and the last chunk
+					 * also contains part of the data we do not want to extract.
+					 * 
+					 * This may occurs when the received chunk length is not a multiple of the given `bytes`.
+					 * In this case the chunk will contain mixed content:
+					 * - end of the `ExtractedData`.
+					 * - begin of data that we do not want to extract.
+					 */
+					const hasByteLoss = ExtractedData.length > bytesToExtract
+					
+					/**
+					 * A Buffer containing chunk data unrelated with the requested extraction data.
+					 * 
+					 */
+					const bytesLoss = hasByteLoss ? ExtractedData.subarray( bytesToExtract ) : undefined
+					
+					// Cut-off extracted data to make sure it exactly matches the requested `bytes`.
+					ExtractedData = ExtractedData.subarray( 0, bytesToExtract )
+					
+					if ( bytesLoss ) {
+						// send rest of the chunk to the next piped destination.
+						this.push( bytesLoss, encoding )
 					}
 
-
-					/**
-					 * Resolve the Promise so user can immediately obtain the `ExtractedData`.
-					 * 
-					 * This cover an edge case scenario where given `bytes` to extract is less than or equal to `0`.
-					 */
 					if ( ! resolved && ExtractedData.length === bytesToExtract ) {
+						// resolve the Promise so user can immediately obtain the `ExtractedData`.
 						resolved = true
 						resolve( [ ExtractedData, this ] )
 					}
 
-
-					bytesRead += chunk.length
-
-					// keep pushing un-modified data to the next piped destination.
-					this.push( chunk, encoding )
-
-					// loop-back
+					// early return with loop-back.
 					return callback()
-				},
-				final( callback ) {
-					if ( ExtractedData.length < bytesToExtract ) {
-						return callback(
-							new Error(
-								'The extracted data Buffer length is less than the expected length. This may be caused if given input data length is less than the data length you want to extract.'
-							)
+				}
+
+
+				/**
+				 * Resolve the Promise so user can immediately obtain the `ExtractedData`.
+				 * 
+				 * This cover an edge case scenario where given `bytes` to extract is less than or equal to `0`.
+				 */
+				if ( ! resolved && ExtractedData.length === bytesToExtract ) {
+					resolved = true
+					resolve( [ ExtractedData, this ] )
+				}
+
+
+				bytesRead += chunk.length
+
+				// keep pushing un-modified data to the next piped destination.
+				this.push( chunk, encoding )
+
+				// loop-back
+				return callback()
+			},
+			final( callback ) {
+				if ( ExtractedData.length < bytesToExtract ) {
+					return callback(
+						new Error(
+							'The extracted data Buffer length is less than the expected length. This may be caused if given input data length is less than the data length you want to extract.'
 						)
-					}
-					return callback()
-				},
-			} )
-
-			transform.on( 'error', reject )
-
-			input.on( 'error', error => {
-				transform.destroy( error )
-				reject( error )
-			} )
-
-			input.pipe( transform )
-
+					)
+				}
+				return callback()
+			},
 		} )
-	)
 
-}
+		transform.on( 'error', reject )
+
+		input.on( 'error', error => {
+			transform.destroy( error )
+			reject( error )
+		} )
+
+		input.pipe( transform )
+
+	} )
+)
